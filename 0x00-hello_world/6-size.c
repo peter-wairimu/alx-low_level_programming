9197015eb452f9d622de071a5f06ba17796d9df6
peter-wairimu <peter.wairimu@student.moringaschool.com>
2022-03-10 14:36:59 +0300

    add size file

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Return:  0 if the program is run successfully else return 1
+ */
+int main(void)
+{
+printf("size of a char: %d byte(s)\n", sizeof(char));
+printf("size of an int: %d byte(s)\n", sizeof(int));
+printf("size of a long int: %d byte(s)\n", sizeof(long int));
+printf("size of a long long int: %d byte(s)\n", sizeof(long long int));
+printf("size of a float: %d byte(s)\n", sizeof(float));
+}
