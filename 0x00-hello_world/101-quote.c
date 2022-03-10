fa8126f52b964b5257efff7cad5bc6087f57ba9c
peter-wairimu <peter.wairimu@student.moringaschool.com>
2022-03-10 15:25:52 +0300

    add advanced question

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
 */
 int main(void)
 {
-char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(1, str, sizeof(str));
+char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, str, 59);
 return (1);
 }
