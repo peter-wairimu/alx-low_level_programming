1de04ac58c91d317853434b94a617f42284c24bb
peter-wairimu <peter.wairimu@student.moringaschool.com>
2022-03-11 15:05:59 +0300

    update files

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints all possible combinations of single-digit numbers.
+ *
+ * Return: if runs successfully return 0
+ */
+int main(void)
+{
+int i;
+for (i = 48; i < 58; i++)
+{
+putchar(i);
+if (i != 57)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
