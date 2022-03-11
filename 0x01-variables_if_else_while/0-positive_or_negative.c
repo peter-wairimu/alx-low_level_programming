acb95fe79afab8b96632bbb944be58088e40b7d2
peter-wairimu <peter.wairimu@student.moringaschool.com>
2022-03-11 12:13:35 +0300

    update file one

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,18 +8,15 @@
  */
 int main(void)
 {
-int i;
+int n;
+
 srand(time(0));
-i = rand() - RAND_MAX / 2;
-if (i > 0)
-{
-printf("%d is positive\n", i);
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+printf("%d is positive\n", n);
+else if (n == 0)
+printf("%d is zero\n", n);
+else if (n < 0)
+printf("%d is negative\n", n);
+return (0);
 }
-else if (i < 0)
-{
-printf("%d is zero\n", i);
-}
-else
-{
-printf("%d is negative\n", i);
-}}
