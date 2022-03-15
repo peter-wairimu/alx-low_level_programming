ada5b99259f6590b830fa46fa8aa457d4b773f0b
peter-wairimu <peter.wairimu@student.moringaschool.com>
2022-03-15 22:58:25 +0300

    initial commit

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,19 +2,18 @@
 /**
  * main - Prints natural numbers below 1024 that are
  * multiplies of 3 or 5
- * 
  * Return: Always 0
  *
  */
 int main(void)
 {
-    int a, b;
+int a, b;
 
-    for (a = 1; a < 1024; a++)
-    {
-        if ((a % 3) == 0 || (a % 5) == 0)
-            b += a;
-    }
-    printf("%d\n", b);
-    return (0);
+for (a = 1; a < 1024; a++)
+{
+if ((a % 3) == 0 || (a % 5) == 0)
+b += a;
+}
+printf("%d\n", b);
+return (0);
 }
