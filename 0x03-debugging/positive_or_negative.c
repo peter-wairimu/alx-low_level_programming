69c92c7eda620c47e75900583f4282ac4972bee4
peter-wairimu <peter.wairimu@student.moringaschool.com>
2022-03-16 14:05:12 +0300

    update file

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,22 +1,16 @@
-#include <stdlib.h>
-#include <time.h>
 #include "main.h"
-
 /**
- * main - Entry point
+ * positive_or_negative - prints if integer positive or negative
+ * @i: integer to be tested
  *
- * Return: if run successfully 0
+ * Return: void
  */
 void positive_or_negative(int i)
 {
-
-srand(time(0));
-i = rand() - RAND_MAX / 2;
-if (i > 0)
-printf("%d is positive\n", i);
-else if (i == 0)
-printf("%d is zero\n", i);
-else if (i < 0)
-printf("%d is negative\n", i);
-
+    if (i > 0)
+        printf("%d is positive\n", i);
+    else if (i < 0)
+        printf("%d is negative\n", i);
+    else
+        printf("%d is zero\n", i);
 }
