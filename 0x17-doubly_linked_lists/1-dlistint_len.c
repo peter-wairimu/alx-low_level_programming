771a0437b9c54678421baeac8a060c7bd090eb74
peter-wairimu <peter.wairimu@student.moringaschool.com>
2022-06-09 20:09:26 +0300

    initial commit

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -9,19 +9,21 @@
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-int count;
-count = 0;
+	int count;
 
-if(h == NULL)
-return (count);
-    
-while(h->prev != NULL)
-h = h->prev;
-    
-while(h != NULL)
-{
-count++;
-h = h->next;
-}
-return (count);
+	count = 0;
+
+	if (h == NULL)
+		return (count);
+
+	while (h->prev != NULL)
+		h = h->prev;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+
+	return (count);
 }
