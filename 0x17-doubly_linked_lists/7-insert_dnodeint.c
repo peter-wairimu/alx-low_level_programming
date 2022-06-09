fe0ec261dbd72b7da91f9776bdc228460477f4d1
peter-wairimu <peter.wairimu@student.moringaschool.com>
2022-06-09 20:28:47 +0300

    initial commit

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,47 +11,44 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-dlistint_t *new_node;
-dlistint_t *current;
-dlistint_t *prev;
-unsigned int i;
+	dlistint_t *new;
+	dlistint_t *head;
+	unsigned int i;
 
-if (h == NULL)
-return (NULL);
+	new = NULL;
+	if (idx == 0)
+		new = add_dnodeint(h, n);
+	else
+	{
+		head = *h;
+		i = 1;
+		if (head != NULL)
+			while (head->prev != NULL)
+				head = head->prev;
+		while (head != NULL)
+		{
+			if (i == idx)
+			{
+				if (head->next == NULL)
+					new = add_dnodeint_end(h, n);
+				else
+				{
+					new = malloc(sizeof(dlistint_t));
+					if (new != NULL)
+					{
+						new->n = n;
+						new->next = head->next;
+						new->prev = head;
+						head->next->prev = new;
+						head->next = new;
+					}
+				}
+				break;
+			}
+			head = head->next;
+			i++;
+		}
+	}
 
-current = *h;
-prev = NULL;
-
-if (idx == 0)
-{
-new_node = malloc(sizeof(dlistint_t));
-if (new_node == NULL)
-return (NULL);
-new_node->n = n;
-new_node->next = current;
-new_node->prev = NULL;
-if (current != NULL)
-current->prev = new_node;
-*h = new_node;
-return (new_node);
-}
-
-for (i = 0; i < idx; i++)
-{
-if (current == NULL)
-return (NULL);
-prev = current;
-current = current->next;
-}
-
-new_node = malloc(sizeof(dlistint_t));
-if (new_node == NULL)
-return (NULL);
-new_node->n = n;
-new_node->next = current;
-new_node->prev = prev;
-prev->next = new_node;
-if (current != NULL)
-current->prev = new_node;
-return (new_node);
+	return (new);
 }
