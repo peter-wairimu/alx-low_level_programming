c4980b94719f58ea9140288da28250e07b4d4bb0
peter-wairimu <peter.wairimu@student.moringaschool.com>
2022-10-04 14:38:19 +0300

    update linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -12,12 +12,13 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
 
-	if (array == NULL)
+	if (!array || size == 0)
 		return (-1);
+
 	for (i = 0; i < size; i++)
 	{
-		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-		if (value == array[i])
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
 			return (i);
 	}
 	return (-1);
